9c8574c3d9ea965c929ad140264e178dd3314d58
spector-armstrong <franklinluvv@gmail.com>
2025-11-01 22:40:19 +0300

    stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+const root = document.querySelector('body')
+
+const background = document.createElement('div')
+background.setAttribute('class', 'background')
+background.style.backgroundColor = "1A2A4F"
+
+
+root.appendChild(background)
