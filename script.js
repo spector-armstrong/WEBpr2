10b27f7737079a7bebf403c481a6d9080a4636fc
spector-armstrong <franklinluvv@gmail.com>
2025-11-01 23:04:43 +0300

    brother may i have some oats

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const root = document.querySelector('body')
+const root = document.querySelector('body');
 
-const background = document.createElement('div')
-background.setAttribute('class', 'background')
-background.style.backgroundColor = "1A2A4F"
+const background = document.createElement('div');
+background.setAttribute('class', 'background');
+background.style.height = "100vh";
+background.style.backgroundColor = "#1a2a4f";
+root.appendChild(background);
 
-
-root.appendChild(background)
+const insideBlock = document.createElement('div');
+insideBlock.setAttribute('class', 'inside-block');
+insideBlock.style.backgroundColor = "#F7A5A5";
+insideBlock.style.width = "100vw";
+insideBlock.style.margin = "3% 3%";
+insideBlock.style.borderRadius = "25px"
+background.appendChild(insideBlock);
